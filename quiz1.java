c16e5d3f59eb7b074f7d56aadb48802b31acb33c
65022443 <119996082+skyyyx@users.noreply.github.com>
2023-07-13 10:31:59 +0700

    Update quiz1.java

--- quiz1.java
+++ quiz1.java
@@ -1,34 +1,56 @@
-class Main {
-    public static void main(String[] args) {
-
-    }
-}
-
 class Student {
+    private int id;
+    private String name;
+    private int admissionYear;
+    private double gpa;
 
-    private int ID;
-    private String Name;
-    private int AddmissionYear;
-    private double GPA;
-
-    Student(int ID, String Name, int AddmissionYear, double GPA) {
-        this.ID = ID;
-        this.Name = Name;
-        this.AddmissionYear = AddmissionYear;
-        this.GPA = GPA;
+    public static void main(String[] args) {
+        Student student = new Student(65022443, "sky", 2023, 2.50);
     }
 
-    public void ShowDetails() {
-        System.out.println("ID : " + this.ID);
-        System.out.println("Name : " + this.Name);
-        System.out.println("Addmission Year : " + this.AddmissionYear);
-        System.out.println("GPA : " + this.GPA);
+    public Student(int id, String name, int admissionYear, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.admissionYear = admissionYear;
+        this.gpa = gpa;
     }
 
-    public void Setting(int ID, String Name, int AddmissionYear, double GPA) {
-        this.ID = ID;
-        this.Name = Name;
-        this.AddmissionYear = AddmissionYear;
-        this.GPA = GPA;
+    public void showDetails() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Admission Year: " + admissionYear);
+        System.out.println("GPA: " + gpa);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAdmissionYear() {
+        return admissionYear;
+    }
+
+    public void setAdmissionYear(int admissionYear) {
+        this.admissionYear = admissionYear;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
     }
 }
